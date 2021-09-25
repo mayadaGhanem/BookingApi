4b2c030ee8e818d2195c7898a27b57a10ba2190a
mayadaGhanem <mayada.hassangh6@gmail.com>
2021-09-25 02:29:25 +0200

    add graphiql

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
+
 const app = express();
+
 app.use(
   "/api",
   graphqlHttp({
     schema: null, /// all schema
     rootValue: {}, /// all resolvers
+    graphiql: true, // to get Gui of Graphql 
   })
 );
+
 app.use(bodyParser.json());
+// listen to port 3030
 app.listen(3030);
