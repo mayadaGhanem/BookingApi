1e8db2a0089a3612c974d85f1f0a04cf7c500792
mayadaHassan <Mayada.ghanem@exsyssolutions.com>
2021-10-02 01:49:11 +0200

    add booking api

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/events");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+const schema = require("./graphql/schema");
+const resolvers = require("./graphql/resolvers");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema, /// all schema
+    rootValue: resolvers,
+    /// all resolvers
+    graphiql: true, // to get Gui of Graphql
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.oc8zl.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`
+  )
+  .then((res) =>
+    // listen to port 3030   +
+    app.listen(3030)
+  )
+  .catch((e) => {
+    console.log(e);
+  });
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+const Event = require("../..//models/events");
+const User = require("../..//models/user");
+const bcrypt = require("bcryptjs");
+const user = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    return {
+      ...user._doc,
+      createdEvents: event.bind(this, user._doc.createdEvents),
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
+const event = async (eventIds) => {
+  try {
+    const events = await Event.find({ createdEvents: { $in: eventIds } });
+    return events.map((event) => ({
+      ...event._doc,
+      creator: user.bind(this, event._doc.creator),
+      date: new Date(event._doc.date).toISOString(),
+    }));
+  } catch (e) {
+    console.log(e);
+  }
+};
+const resolvers = {
+  events: async () => {
+    try {
+      const res = await Event.find({});
+      return res.map((event) => ({
+        ...event._doc,
+        creator: user.bind(this, event._doc.creator),
+      }));
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  users: async () => {
+    try {
+      const res = await User.find({});
+      return res.map((user) => ({ ...user._doc }));
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  createEvent: async (args) => {
+    try {
+      let createdEvent;
+      let event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date),
+        creator: "614f9083993de1fcc6c0e319",
+      });
+      const newEvent = await event.save();
+      createdEvent = {
+        ...newEvent._doc,
+        date: new Date(newEvent._doc.date).toISOString(),
+        creator: user.bind(this, newEvent._doc.creator),
+      };
+      const userExist = await User.findById("614f9083993de1fcc6c0e319");
+
+      if (!userExist) {
+        throw new Error("The User Not Found");
+      }
+      userExist.createdEvents.push(event);
+      await userExist.save();
+      return createdEvent;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  createUser: async (args) => {
+    try {
+      const userExist = await User.findOne({ email: args.userInput.email });
+      if (userExist) {
+        throw new Error("The User Already Exists");
+      }
+      const hashPassword = await bcrypt.hash(args.userInput.password, 12);
+      const user = new User({
+        password: hashPassword,
+        email: args.userInput.email,
+      });
+      const newUser = await user.save();
+      return {
+        ...newUser._doc,
+        password: null,
+        createdEvents: event.bind(this, newUser._doc.createdEvents),
+      };
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
+module.exports = resolvers;
